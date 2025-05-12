b1477a887150172e0a23b5346ed58c7ed94fa69c
unknown <sapna.alex03@gmail.com>
2025-05-12 13:27:53 +0530

    Commit

--- Pet-service/server.js
+++ Pet-service/server.js
@@ -1,29 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
 const userRoutes = require("./routes/userRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
 
-dotenv.config();
-const app = express();
-app.use(express.json());
-
-app.get("/",(req, res)=>{
+app.get("/", (req, res)=>{
   res.send("Server is live!");
 })
+const app = express();
+app.use(express.json());
 
 app.use("/register", userRoutes);
 app.use("/appointments", appointmentRoutes);
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("DB Connection Error:", err);
-  });
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+  console.log("MongoDB Connected");
+
+})
+.catch((err)=>{
+  console.error("DB Connection Error: ",err);
+});
+
+app.listen(process.env.PORT, ()=>{
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
